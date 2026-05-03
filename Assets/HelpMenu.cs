9dba41edc983a4d48b4476e729533fa3e5e63fd6
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-05-03 02:33:20 +0200

    script del tabmenu

---
+++ Assets/HelpMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HelpMenu : MonoBehaviour
+{
+    VisualElement contenidoCharacter;
+    VisualElement contenidoMovement;
+    VisualElement contenidoAttack;
+
+    VisualElement navCharacter;
+    VisualElement navMovement;
+    VisualElement navAttack;
+
+    private void NoContenido()
+    {
+        contenidoCharacter.style.display = DisplayStyle.None;
+        contenidoMovement.style.display = DisplayStyle.None;
+        contenidoAttack.style.display = DisplayStyle.None;
+    }
+
+    private void OnEnable()
+    {
+        UIDocument uiDoc = GetComponent<UIDocument>();
+        VisualElement root = uiDoc.rootVisualElement;
+
+        VisualElement nav = root.Q<VisualElement>("Pestanias");
+        VisualElement body = root.Q<VisualElement>("Contenido");
+
+        contenidoCharacter = root.Q<VisualElement>("CharacterTab");
+        contenidoMovement = root.Q<VisualElement>("MovementTab");
+        contenidoAttack = root.Q<VisualElement>("AttackTab");
+
+        navCharacter = root.Q<VisualElement>("CharacterPage");
+        navMovement = root.Q<VisualElement>("MovementPage");
+        navAttack = root.Q<VisualElement>("AttackPage");
+
+        navCharacter.RegisterCallback<ClickEvent>((evt) =>
+        {
+            Debug.Log("Azul");
+            NoContenido();
+            contenidoCharacter.style.display = DisplayStyle.Flex;
+        });
+        navMovement.RegisterCallback<ClickEvent>((evt) =>
+        {
+            Debug.Log("Verde");
+            NoContenido();
+            contenidoMovement.style.display = DisplayStyle.Flex;
+        });
+        navAttack.RegisterCallback<ClickEvent>((evt) =>
+        {
+            Debug.Log("Amarillo");
+            NoContenido();
+            contenidoAttack.style.display = DisplayStyle.Flex;
+        });
+
+    }
+}
