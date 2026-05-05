98436f2dabeb8a809d349edf39dfc9fbb8e05799
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-05-05 13:21:27 +0200

    Conectar end y start

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EndScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    StartScreen _startScreen;
+    SetupScreen _setupScreen;
+    VisualElement _root;
+
+    Button _retryButton;
+    Button _newGameButton;
+
+    void OnEnable()
     {
-        
+        _startScreen = GetComponent<StartScreen>();
+
+        _root = GetComponent<UIDocument>().rootVisualElement;
+
+        // activar endscreen
+        _root.Q("EndScreen").style.display = DisplayStyle.Flex;
+
+        // botones
+        _retryButton = _root.Q<Button>("RetryButton");
+        _newGameButton = _root.Q<Button>("NewGameButton");
+
+        _retryButton.RegisterCallback<ClickEvent>(OnRetry);
+
+        _newGameButton.RegisterCallback<ClickEvent>(OnNewGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnRetry(ClickEvent ev)
     {
-        
+        _startScreen.enabled = true;
+
+        this.enabled = false;
+    }
+
+    void OnNewGame(ClickEvent ev)
+    {
+        _setupScreen.enabled = true;
+
+        this.enabled = false;
+
+    }
+
+    void OnDisable()
+    {
+        _root.Q("EndScreen").style.display = DisplayStyle.None;
+
     }
 }
