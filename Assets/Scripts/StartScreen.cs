01fb2c5d35a0e9876d39e683ef025120c6d9aef9
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-05-06 02:23:42 +0200

    [StartScreen] Añadir efectos hover y click

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -5,7 +5,8 @@ public class StartScreen : MonoBehaviour
 {
     SetupScreen _setupScreen;
     VisualElement _root;
-    
+    Button _startButton;
+
     void OnEnable()
     {
         _setupScreen = GetComponent<SetupScreen>();
@@ -15,8 +16,22 @@ public class StartScreen : MonoBehaviour
         // activar el start screen
         _root.Q("StartScreen").style.display = DisplayStyle.Flex;
         // coger boton
-        VisualElement startButton = _root.Q<Button>();
-        startButton.RegisterCallback<ClickEvent>(OnStart);
+        _startButton = _root.Q<Button>("StartButton");
+
+        // CLICK
+        _startButton.RegisterCallback<ClickEvent>(OnStart);
+
+        // HOVER ENTER
+        _startButton.RegisterCallback<MouseEnterEvent>(OnHoverEnter);
+
+        // HOVER EXIT
+        _startButton.RegisterCallback<MouseLeaveEvent>(OnHoverExit);
+
+        // PRESSED
+        _startButton.RegisterCallback<MouseDownEvent>(OnPressed);
+
+        // RELEASE
+        _startButton.RegisterCallback<MouseUpEvent>(OnReleased);
     }
 
     // cambiar al setup screen cuando empieza el juego
@@ -24,6 +39,25 @@ public class StartScreen : MonoBehaviour
     {
         _setupScreen.enabled = true;
         this.enabled = false;
+    }
+
+    void OnHoverEnter(MouseEnterEvent ev)
+    {
+        _startButton.AddToClassList("classic-button-hover");
+    }
+
+    void OnHoverExit(MouseLeaveEvent ev)
+    {
+        _startButton.RemoveFromClassList("classic-button-hover");
+    }
+    void OnPressed(MouseDownEvent ev)
+    {
+        _startButton.AddToClassList("classic-button-pressed");
+    }
+
+    void OnReleased(MouseUpEvent ev)
+    {
+        _startButton.RemoveFromClassList("classic-button-pressed");
     }
 
     void OnDisable()
