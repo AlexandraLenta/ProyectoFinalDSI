a065d64fb1a30580c2b8b00ca8e4e75c88035f3e
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-05-06 02:40:30 +0200

    cambios sin subir

--- Assets/Scripts/GameScreen.cs
+++ Assets/Scripts/GameScreen.cs
@@ -8,12 +8,17 @@ public class GameScreen : MonoBehaviour
     VisualElement _root;
 
     Button _helpButton;
+    Button _moveButton;
+    Button _attackButton;
 
     void OnEnable()
     {
         _root = GetComponent<UIDocument>().rootVisualElement;
 
         _root.Q("GameScreen").style.display = DisplayStyle.Flex;
+
+        _moveButton = _root.Q<Button>("MoveButton");
+        _attackButton = _root.Q<Button>("AttackButton");
     }
 
 
